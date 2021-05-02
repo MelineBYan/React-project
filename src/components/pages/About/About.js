7c04df1184deb4ecd6c07068be7469c60ca270ad
MelineBYan <meline.barseghyan.91@mail.ru>
2021-05-02 16:24:07 +0400

    about page

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Button } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import styles from "./About.module.css";
 import PropTypes from "prop-types";
 
