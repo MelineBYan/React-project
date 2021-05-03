3d7c8bf234af033c114729eda229cefd241b36ca
MelineBYan <meline.barseghyan.91@mail.ru>
2021-05-03 21:22:31 +0400

    for netlify

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Button, Col } from "react-bootstrap";
 import styles from "./About.module.css";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
 
 const About = () => {
   return (
-    <Container className="mt-5 my-3 bg-light text-light bg-dark shadow-lg p-3">
+    <Container className="mt-5  bg-light text-light bg-dark shadow-lg p-3">
       <h1 className={styles.title}>
         About me<span className={styles.span}>About me</span>
       </h1>
@@ -20,7 +22,9 @@ const About = () => {
             I am a future Web developer. I have been interested in the
             programming for a long time. I took this opportunity to learn and
             acquire a new profession․ I'm always in for learning new stuff,
-            seeking to improve and grow as a professional and as a person.
+            seeking to improve and grow as a professional and as a person. Right
+            now I am in the seek of an internship position or an entry level
+            position with an exciting company.
           </p>
         </Row>
         <Row>
@@ -39,12 +43,44 @@ const About = () => {
             </p>
           </div>
         </Row>
-        <Row></Row>
+        <Row>
+          <Col>
+            <Button variant="info" className={styles.btn}>
+              <Link
+                to="/CV-Meline-Barseghyan.pdf"
+                target="_blank"
+                download
+                style={{
+                  color: "white",
+                  textDecoration: "none",
+                }}
+              >
+                Download CV
+                <FontAwesomeIcon icon={faDownload} className="text-dark" />
+              </Link>
+            </Button>
+          </Col>
+        </Row>
       </div>
-      <Footer>
-        {/* <FontAwesomeIcon icon={} className="text-dark h5" /> */}
-        {/* <FontAwesomeIcon icon={faLinkedin} className="text-dark h5" /> */}
-      </Footer>
+      <div className="d-flex">
+        <Button
+          variant="dark"
+          className="ml-auto"
+          onClick={() =>
+            (window.location =
+              "https://www.linkedin.com/in/meline-barseghyan-62363a207/")
+          }
+        >
+          <FontAwesomeIcon icon={faLinkedin} className="text-light h5" />
+        </Button>
+        <Button
+          variant="dark"
+          className="mr-auto"
+          onClick={() => (window.location = "https://github.com/MelineBYan")}
+        >
+          <FontAwesomeIcon icon={faGithub} className="text-light h5" />
+        </Button>
+      </div>
     </Container>
   );
 };
